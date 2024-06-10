a125807c76c6c06a737ceb432f5f2899c1417b1c
SavioNicodemos <nicodemosgcosta@gmail.com>
2024-06-10 12:55:16 +0100

    fix: update Swagger route prefix to '/docs' in routes files too

--- src/routes.php
+++ src/routes.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use AutoSwagger\Docs\Http\Controllers\SwaggerController;
 
 if (Config::get('swagger.enable', true)) {
-    Route::prefix(config('swagger.path', '/documentation'))->group(static function () {
+    Route::prefix(Config::get('swagger.path', '/docs'))->group(static function () {
         Route::get('', [SwaggerController::class, 'api']);
         Route::get('content', [SwaggerController::class, 'documentation']);
     });
